fe200363accbf91056ac0aa7e5afe779dedd360d
zzc-1998 <58689334+zzc-1998@users.noreply.github.com>
2021-07-16 20:37:35 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
 from feature_extract import get_feature_vector
+import time
+
 #demo
 objpath = "hhi_5.ply"
+start = time.time()
 features = get_feature_vector(objpath)
+end = time.time()
+time_cost = end-start
+
 
 #show the features
 cnt = 0
@@ -9,3 +15,4 @@ for feature_domain in ['l','a','b','curvature','anisotropy','linearity','planari
     for param in ["mean","std","entropy","ggd1","ggd2","aggd1","aggd2","aggd3","aggd4","gamma1","gamma2"]:
         print(feature_domain + "_" + param + ": " + str(features[cnt]))
         cnt = cnt + 1
+print("Cost " + str(time_cost) + " sec.")
