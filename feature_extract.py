324953d91ae8a5c38df0b9a0b914ac835f5450a4
zzc-1998 <58689334+zzc-1998@users.noreply.github.com>
2021-07-16 20:27:25 +0800

    Update feature_extract.py

--- feature_extract.py
+++ feature_extract.py
@@ -44,13 +44,4 @@ def get_feature_vector(objpath):
       nss_params = nss_params + [i for item in params for i in item]
   return nss_params
 
-#demo
-objpath = "hhi_5.ply"
-features = get_feature_vector(objpath)
 
-#show the features
-cnt = 0
-for feature_domain in ['l','a','b','curvature','anisotropy','linearity','planarity','sphericity']:
-    for param in ["mean","std","entropy","ggd1","ggd2","aggd1","aggd2","aggd3","aggd4","gamma1","gamma2"]:
-        print(feature_domain + "_" + param + ": " + str(features[cnt]))
-        cnt = cnt + 1
